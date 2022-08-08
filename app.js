2531bd406ca30ae2cae3aa68c13a8e77ca217b69
SheonXJ <s87462830@gmail.com>
2022-08-08 12:51:58 +0800

    project init

---
+++ app.js
@@ -0,0 +1,16 @@
+// load package
+const express = require('express')
+
+// setting server
+const PORT = process.env.PORT || 3000
+const app = express()
+
+// setting route
+app.get('/', (req, res) => {
+  res.send('test')
+})
+
+// activate server
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
